69c423bac716943408d523349104d6e3e1657b6d
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2022-01-26 11:23:48 +0800

    Add files via upload

---
+++ Math-Challenge/math-1.0.py
@@ -0,0 +1,9 @@
+import math_tz
+print("欢迎使用数学挑战程序！")
+
+a=input("乘法挑战请按1,加法挑战请按2，除法挑战请按3，减法挑战请按4,退出请按0。 | Press 1 for multiplication challenge, 2 for addition challenge, 3 for division challenge, 4 for subtraction challenge, and 0 for exit.   请选择...|Please choose...   ")
+a=int(a)
+if a==1:
+    math_tz.chengfa()
+#    print(math_tz.chengfa(a))
+#   ans=input()
---
+++ Math-Challenge/math_tz.py
@@ -0,0 +1,19 @@
+import random
+def chengfa():
+    while True:
+        a=random.randint(1,20)
+        b=random.randint(1,20)
+        c=a*b
+        print(a)
+        print("×")
+        print(b)
+        d=input("等于几:  ")
+        d=int(d)
+        if d==c:
+            print("正确!|You are right!")
+            print("-----------------------------------------")
+        else:
+            print("错误!|That's wrong!")
+            print("正确答案为：|The correct answer is:")
+            print(c)
+            print("-----------------------------------------")
